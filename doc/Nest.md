30f6bbb5962f3a6dde89995c19a51f1e1eb7228f
fox.wang <fox3000wang@hotmail.com>
2020-07-07 11:49:59 +0800

    1st commit

---
+++ doc/Nest.md
@@ -0,0 +1,17 @@
+# Nest 学习笔记
+
+## hello world
+
+```shell
+npm i -g @nestjs/cli
+
+nest new project-name
+
+npm run start
+
+# http://localhost:3000/
+```
+
+## 重要概念
+
+### 控制器
---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+/*
+  Controller 控制器负责处理传入的请求，并返回对客户端的响应
+*/
+
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
