9d731014d17e5dfd84db7a71e745be8351e85ca1
fox.wang <fox3000wang@hotmail.com>
2020-07-07 13:46:35 +0800

    初步熟悉nest g工具

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VitaModule } from './vita/vita.module';
+import { UserModule } from './user/user.module';
 
 // 根Module
 @Module({
-  imports: [VitaModule],
+  imports: [VitaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
