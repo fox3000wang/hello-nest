520ba11c9edb6d5792978ccd3ea82d606df2cb46
fox.wang <fox3000wang@hotmail.com>
2020-07-07 13:57:20 +0800

    先提交吧，mac没电了

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,15 @@
 /*
   Controller 控制器负责处理传入的请求，并返回对客户端的响应
+
+  @Request()	req
+  @Response(), @Res()*	res
+  @Next()	next
+  @Session()	req.session
+  @Param(key?: string)	req.params / req.params[key]
+  @Body(key?: string)	req.body / req.body[key]
+  @Query(key?: string)	req.query / req.query[key]
+  @Headers(name?: string)	req.headers / req.headers[name]
+  @Ip()	req.ip
 */
 
 import { Controller, Get } from '@nestjs/common';
